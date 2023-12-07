b72ac8d658326185298c92bf0c251994120c4f0b
Shoon Lae Yee <110923904+shoon007@users.noreply.github.com>
2023-12-07 15:56:54 +0630

    Update master.blade.php

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -8,7 +8,6 @@
      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" />
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
-     <link rel="stylesheet" href="styles.css" />
      <title>Product Managing Dashboard</title>
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
          integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
@@ -59,94 +58,49 @@
          integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
          crossorigin=""></script>
      <script src="https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.js"></script>
+     {{--   custom js  --}}
+     <script src="{{ asset('js/master.js') }}"></script>
+     @yield('script')
 
-       {{--   /* custom js */ --}}
-         <script src = "{{ asset('js/master.js') }}" ></script>
+     <script>
+         //changing the publish status with ajax
+         $(document).ready(function() {
+             $('.statusChange').change(function() {
+                 $currentRole = $(this).val();
+                 $checked = $(this).is(":checked");
+                 $parentNode = $(this).parents('tbody tr td label');
+                 $productId = $parentNode.find('#productId').val();
+                 $.ajax({
+                     type: 'get',
+                     url: 'http://127.0.0.1:8000/admin/status/change',
+                     data: {
+                         'role': $currentRole,
+                         'productId': $productId,
+                         'checked': $checked
+                     },
+                     dataType: 'json',
+                 })
 
-<script>
-     //changing the publish status with ajax
-     $(document).ready(function() {
-         $('.statusChange').change(function() {
-             $currentRole = $(this).val();
-             $checked = $(this).is(":checked");
-             $parentNode = $(this).parents('tbody tr td label');
-             $productId = $parentNode.find('#productId').val();
-             $.ajax({
-                 type: 'get',
-                 url: 'http://127.0.0.1:8000/admin/status/change',
-                 data: {
-                     'role': $currentRole,
-                     'productId': $productId,
-                     'checked': $checked
-                 },
-                 dataType: 'json',
+             })
+
+
+             $('.categoryStatus').change(function() {
+                 $currentRole = $(this).val();
+                 $checked = $(this).is(":checked");
+                 $parentNode = $(this).parents('tbody tr td label');
+                 $categoryId = $parentNode.find('#categoryId').val();
+                 $.ajax({
+                     type: 'get',
+                     url: 'http://127.0.0.1:8000/admin/category/status',
+                     data: {
+                         'role': $currentRole,
+                         'categoryId': $categoryId,
+                         'checked': $checked
+                     },
+                     dataType: 'json',
+                 })
+
              })
 
          })
-
-
-         $('.categoryStatus').change(function() {
-             $currentRole = $(this).val();
-             $checked = $(this).is(":checked");
-             $parentNode = $(this).parents('tbody tr td label');
-             $categoryId = $parentNode.find('#categoryId').val();
-             $.ajax({
-                 type: 'get',
-                 url: 'http://127.0.0.1:8000/admin/category/status',
-                 data: {
-                     'role': $currentRole,
-                     'categoryId': $categoryId,
-                     'checked': $checked
-                 },
-                 dataType: 'json',
-             })
-
-         })
-
-     })
-
-     var map_init = L.map('map', {
-         center: [9.0820, 8.6753],
-         zoom: 8
-     });
-     var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-     }).addTo(map_init);
-     L.Control.geocoder().addTo(map_init);
-     if (!navigator.geolocation) {
-         console.log("Your browser doesn't support geolocation feature!")
-     } else {
-         setInterval(() => {
-             navigator.geolocation.getCurrentPosition(getPosition)
-         }, 5000);
-     };
-     var marker, circle, lat, long, accuracy;
-
-     function getPosition(position) {
-         // console.log(position)
-         lat = position.coords.latitude
-         long = position.coords.longitude
-         accuracy = position.coords.accuracy
-
-         if (marker) {
-             map_init.removeLayer(marker)
-         }
-
-         if (circle) {
-             map_init.removeLayer(circle)
-         }
-
-         marker = L.marker([lat, long])
-         circle = L.circle([lat, long], {
-             radius: accuracy
-         })
-
-         var featureGroup = L.featureGroup([marker, circle]).addTo(map_init)
-
-         map_init.fitBounds(featureGroup.getBounds())
-
-         console.log("Your coordinate is: Lat: " + lat + " Long: " + long + " Accuracy: " + accuracy)
-     }
-
-
-</script>
+     </script>
